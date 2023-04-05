59b7e5d8bfb9e377663e8c73f2900cd7bbb5c490
walterdyna <alexdyna@gmail.com>
2023-04-05 08:41:24 -0300

    bora

--- variaveis.js
+++ variaveis.js
@@ -1,14 +1,24 @@
 //variaveis
 
- var number1 = 1 
+ var number1 = 1  // Não usar o var pois antigamente so tinha ele e tudo era ele, hoje tem novas variaveis
 
  let number2 = 2 
 
- const number3 = 3
- 
- console.log(number1)
+ const number3 = 3  //  - const e um valor fixo não pode mudar --- EX: Valor de pi (3.14)
+
+//  ---Declaração de variavel---
+
+number1 = 10
+number2 = 20
+
+
+// --- Enviar para o console--- 
+
+ console.log(number1) 
  console.log(number2)
  console.log(number3)
+
+//  Obs: Uma variavel não usa "" aspas
 
 
 //  ---Permitido:---
