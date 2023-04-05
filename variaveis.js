c139aab3af0b38811455bbae88456d7be5c78339
walterdyna <alexdyna@gmail.com>
2023-04-05 08:33:24 -0300

    Js pegando fogo

--- variaveis.js
+++ variaveis.js
@@ -1,3 +1,38 @@
 //variaveis
 
+ var number1 = 1 
+
+ let number2 = 2 
+
+ const number3 = 3
  
+ console.log(number1)
+ console.log(number2)
+ console.log(number3)
+
+
+//  ---Permitido:---
+
+// - Iniciar com $ ou com _
+
+// - Colocar Acentos
+
+// - Iniciar com Letras
+
+// - Letras Maiusculas e Minusculas - Porem o sistema e QUEISESINTIVE
+
+// ---Não e Permitido:---
+
+// - Iniciar com Numeros
+
+// - Colocar Espaços
+
+// ---O Ideal---
+
+// - Camel case --- = Camelo --- escrevaDestaFormaAgora
+
+// - Snake case --- = Cobra ---escreva_desta_forma_agora
+
+// - Sempre em Ingles
+
+// - Nome que  fazem sentido - tentar colocar uma variavel que seja de facil entendimento para o proximo e vc.
